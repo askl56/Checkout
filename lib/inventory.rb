4ca656f656154e88f1ff6adaa3659be45ee87723
askl56 <amscott@openmailbox.org>
2015-06-10 19:22:16 +0100

    fixed Inventory location issues

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,10 +1,18 @@
 require 'yaml'
 
+
+INVENTORY_LIST = File.join(File.dirname(__FILE__), "..", "config/", "inventory.yaml")
+
+
 class Inventory
-
   attr_reader :items
-
   def initialize
-    @items = YAML.load_file = 'inventory.yaml'
+    @items = YAML.load_file INVENTORY_LIST
   end
+
+
+  # Inventory is passed directly instead of as a param because
+  # something as important as an inventory file should not be
+  # arbitrary. It should be solidified.
+
 end
