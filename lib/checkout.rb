ff341452cc6c2b0d5b9628ca03c6b8317310b223
askl56 <amscott@openmailbox.org>
2015-06-10 20:49:02 +0100

    refactoring

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -8,18 +8,5 @@ class Checkout
   end
 
   def total
-    @basket.each do |item|
-      if has_rule?(item)
-        adjust_item
-      end
-    end
-  end
-
-  def has_rule?(item)
-    if pricing_rules.include? item
-      true
-    else
-      false
-    end
   end
 end
