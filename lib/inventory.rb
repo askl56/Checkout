103eb696e0b26d0a08f8d937e949b137638b77f9
askl56 <amscott@openmailbox.org>
2015-06-10 18:52:26 +0100

    every day im modularizing

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,7 +1,10 @@
 require 'yaml'
 
 class Inventory
+
+  attr_reader :items
+
   def initialize
-    @items = YAML.load_file = 'inventory.txt'
+    @items = YAML.load_file = 'inventory.yaml'
   end
 end
