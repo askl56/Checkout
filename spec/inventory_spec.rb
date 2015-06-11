81b89960defa51b881b57f40d7e977c2e1ef01d8
askl56 <amscott@openmailbox.org>
2015-06-11 02:09:58 +0100

    added inventory spec

--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -1,13 +1,46 @@
 require 'inventory'
 
 describe Inventory do
-  before(:each) do
-    @inventory = Inventory.new
+  let(:inventory) { Inventory.new }
+
+  context "verify hash data" do
+    describe "expect hash to contain correct symbols" do
+      it "contains the correct code for Strawberries" do
+        expect(inventory.items).to include(:SR1)
+      end
+      it "contains the correct code for Green Tea" do
+        expect(inventory.items).to include(:GR1)
+      end
+      it "contains the correct code for Coffee" do
+        expect(inventory.items).to include(:CF1)
+      end
+    end
+
+    describe "expect hash to contain correct names" do
+      it "contains the correct name for Strawberries" do
+        expect(inventory.items).to include("Strawberries")
+      end
+
+      it "contains the correct name for Green Tea" do
+        expect(inventory.items).to include("Green Tea")
+      end
+
+      it "contains the correct name for Coffee" do
+        expect(inventory.items).to include("Coffee")
+      end
+    end
+
+    describe "expect hash to contain correct prices" do
+      it "contains the correct price for Strawberries" do
+        expect(inventory.items).to include(5.0)
+      end
+      it "contains the correct price for Green Tea" do
+        expect(inventory.items).to include(3.11)
+      end
+
+      it "contains the correct price for Coffee" do
+        expect(inventory.items).to include(11.23)
+      end
+    end
   end
-
-  describe "expect hash to contain all items" do
-
-
-  end
-
 end
