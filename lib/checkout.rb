b91157620e29097cf7758e2a82abf712d47c5d77
askl56 <amscott@openmailbox.org>
2015-06-11 05:10:56 +0100

    refactoring

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,9 +2,11 @@ class Checkout
 
   attr_accessor :basket, :pricing_rules
 
-  def initialize
+  def initialize(basket = [], pricing_rules = [])
 
     # We initialize the items as an empty array.
+
+    @inventory = Inventory.new
 
     @basket = []
     @pricing_rules = pricing_rules
@@ -12,16 +14,17 @@ class Checkout
   end
 
   def scan(item)
-
+  	inventory_item = @inventory.items[item]
+  	raise 'error' unless inventory_item
     # Each item is simply added to the basket using the
     # shovel syntax.
 
-    @basket << item
+    @basket << inventory_item.clone
   end
 
   def total
     PricingRules.bogof(@basket)
     PricingRules.bulk_buy(@basket)
-    @basket.map { |i| i.price }.reduce(:+)
+    @basket.map { |i| i[:price] }.reduce(:+)
   end
 end
--- lib/pricing_rule.rb
+++ lib/pricing_rule.rb
@@ -1,14 +1,13 @@
 class PricingRules
-
   def self.bogof(basket)
     if basket.select { |i| i[:code] == 'GR1' }.count > 2
-      basket.each { |items| items.find_all{ |item| item[:code] == 'GR1' }.each_with_index { |item, i| item[:price] = 0 if i.odd? } }
+      basket.each_with_index { |item, i| item[:price] = 0.00 if i.even? && item[:code] == 'GR1' }
     end
   end
 
   def self.bulk_buy(basket)
     if basket.select { |i| i[:code] == 'SR1' }.count >= 3
-      basket.each { |items| items.each { |item| item[:price] = 4.5 if item[:code] == 'SR1'}}
+      basket.each { |items| items[:price] = 4.5 if items[:code] == 'SR1' }
     end
   end
 end
